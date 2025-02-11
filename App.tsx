a1ec0a6fd4395128d9ece54a7060f9dd8bb75240
dempsalicious <jsahagianco@gmail.com>
2025-02-11 12:35:38 -0500

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,1 +1,58 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import HomeScreen from "./screens/HomeScreen";
+import CommunicationScreen from "./screens/CommunicationScreen";
+import ResourceScreen from "./screens/ResourceScreen";
+import MapScreen from "./screens/MapScreen";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
 
+const Tab = createBottomTabNavigator();
+
+function TabNavigator() {
+  return (
+    <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+          if (route.name === 'Emergencies') {
+            iconName = focused ? 'warning' : 'warning-outline';
+          } else if (route.name === 'Communications') {
+            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
+          } else if (route.name === 'Resources') {
+            iconName = focused ? 'cube' : 'cube-outline';
+          } else if (route.name === 'Map') {
+            iconName = focused ? 'map' : 'map-outline';
+          }
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: '#2F80ED',
+        tabBarInactiveTintColor: 'gray',
+        headerShown: false
+      })}
+    >
+      <Tab.Screen name="Emergencies" component={HomeScreen} />
+      <Tab.Screen name="Communications" component={CommunicationScreen} />
+      <Tab.Screen name="Resources" component={ResourceScreen} />
+      <Tab.Screen name="Map" component={MapScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5'
+  }
+});
